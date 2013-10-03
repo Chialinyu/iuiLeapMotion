f85cb3ced36d0ef64f84e08477606c148f314e8d
Amy <amy19911101@gmail.com>
2013-10-03 18:39:35 +0800

    Initial Commit

---
+++ iuiLeapMotion/main.cpp
@@ -0,0 +1,18 @@
+//
+//  main.cpp
+//  iuiLeapMotion
+//
+//  Created by Amy on 13/10/3.
+//  Copyright (c) 2013年 Amy. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[])
+{
+
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
+
